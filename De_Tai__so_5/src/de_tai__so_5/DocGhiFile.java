0970608cbc8079e2fa00f4213221d756ff7c3ca9
PhuongAnh <phamphuonganh.hvact@gmail.com>
2021-07-30 00:14:06 +0700

    update

---
+++ De_Tai__so_5/src/de_tai__so_5/DocGhiFile.java
@@ -0,0 +1,245 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de_tai__so_5;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DocGhiFile {
+
+    private static final String curentDir = System.getProperty("user.dir");
+    private static final String separator = File.separator;
+    private static final String PATH_FILE_CSV = curentDir + separator + "data" + separator + "KhachHang.csv";
+    private static final String PATH_FILE_CSV_Phong = curentDir + separator + "data" + separator + "Phong.csv";
+    private static final String PATH_FILE_CSV_DATPHONG=curentDir + separator + "data" + separator + "DatPhong.csv";
+
+    public void ghiFile(List<khachHang> list) {
+        FileWriter fw = null;
+        try {
+            File f = new File(PATH_FILE_CSV);
+            if (!f.exists()) {
+                fw = new FileWriter(PATH_FILE_CSV, true);
+                CSVWriter csvWrite = new CSVWriter(fw, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+                String[] header = {"Ma KH", "Ho Ten", "So CMND", "SĐT", "Tuoi", "PTTT"};
+                csvWrite.writeNext(header);
+                for (khachHang item : list) {
+                    csvWrite.writeNext(new String[]{String.valueOf(item.getMaKhachHang()), item.getHoTen(), String.valueOf(item.getCMND()), String.valueOf(item.getSDT()), String.valueOf(item.getTuoi()), item.getPhuongThucThanhToan()});
+
+                }
+            } else {
+                fw = new FileWriter(PATH_FILE_CSV, true);
+                CSVWriter csvWrite = new CSVWriter(fw, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+                for (khachHang item : list) {
+                    csvWrite.writeNext(new String[]{String.valueOf(item.getMaKhachHang()), item.getHoTen(), String.valueOf(item.getCMND()), String.valueOf(item.getSDT()), String.valueOf(item.getTuoi()), item.getPhuongThucThanhToan()});
+
+                }
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(DocGhiFile.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(DocGhiFile.class
+                            .getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
+    }
+
+    public void ghiFilePhong(List<Phong> list) {
+        FileWriter fw = null;
+        try {
+            File f = new File(PATH_FILE_CSV_Phong);
+            if (!f.exists()) {
+                fw = new FileWriter(PATH_FILE_CSV_Phong, true);
+                CSVWriter csvWrite = new CSVWriter(fw, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+                String[] header = {"Ma Phong", "So Tang", "Loại Phòng", "Gia tien"};
+                csvWrite.writeNext(header);
+                for (Phong item : list) {
+                    csvWrite.writeNext(new String[]{String.valueOf(item.getMaPhong()), String.valueOf(item.getSoTang()), String.valueOf(item.getLoaiPhong()), String.valueOf(item.getGiaPhong())});
+
+                }
+            } else {
+                fw = new FileWriter(PATH_FILE_CSV_Phong, true);
+                CSVWriter csvWrite = new CSVWriter(fw, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+                for (Phong item : list) {
+                    csvWrite.writeNext(new String[]{String.valueOf(item.getMaPhong()), String.valueOf(item.getSoTang()), String.valueOf(item.getLoaiPhong()), String.valueOf(item.getGiaPhong())});
+                }
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(DocGhiFile.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(DocGhiFile.class
+                            .getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
+    }
+
+    public List<khachHang> docFile() throws CsvValidationException {
+        FileReader fr = null;
+        List<khachHang> list = new ArrayList<>();
+        try {
+            fr = new FileReader(PATH_FILE_CSV);
+            CSVReader csvReader = new CSVReader(fr);
+            String[] line;
+            line = csvReader.readNext();
+            while ((line = csvReader.readNext()) != null) {
+                khachHang kh = new khachHang(line[0], line[1], Integer.parseInt(line[2]), Integer.parseInt(line[3]), Integer.parseInt(line[4]), line[5]);
+                list.add(kh);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(DocGhiFile.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fr.close();
+            } catch (IOException ex) {
+                Logger.getLogger(DocGhiFile.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        return list;
+    }
+
+    public List<Phong> docFilePhong() throws CsvValidationException {
+        FileReader fr = null;
+        List<Phong> list = new ArrayList<>();
+        try {
+            fr = new FileReader(PATH_FILE_CSV_Phong);
+            CSVReader csvReader = new CSVReader(fr);
+            String[] line;
+            line = csvReader.readNext();
+            while ((line = csvReader.readNext()) != null) {
+                Phong phong = new Phong(line[0], Integer.parseInt(line[1]), Integer.parseInt(line[2]), Double.parseDouble(line[3]));
+                list.add(phong);
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(DocGhiFile.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (fr != null) {
+                try {
+                    fr.close();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(DocGhiFile.class
+                            .getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }
+        return list;
+    }
+ public void ghiFileDatPhong(List<DatPhong> list) {
+     SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        FileWriter fw = null;
+        try {
+            File f = new File(PATH_FILE_CSV_DATPHONG);
+            if (!f.exists()) {
+                fw = new FileWriter(PATH_FILE_CSV_DATPHONG, true);
+                CSVWriter csvWrite = new CSVWriter(fw, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+                String[] header = {"Ma Dat Phong", "Ma Phong", "Ma Khach Hang", "NgayDat", "NgayTRa", "TongTien"};
+                csvWrite.writeNext(header);
+                for (DatPhong item : list) {
+                    csvWrite.writeNext(new String[]{String.valueOf(item.getMaDatPhong()), String.valueOf(item.getMaPhong()),String.valueOf(item.getMaKhachHang()),df.format(item.getNgayDat()) ,df.format(item.getNgayDat()), String.valueOf(item.getTongTien())});
+
+                }
+            } else {
+                fw = new FileWriter(PATH_FILE_CSV_DATPHONG, true);
+                CSVWriter csvWrite = new CSVWriter(fw, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+                for (DatPhong item : list) {
+                    csvWrite.writeNext(new String[]{String.valueOf(item.getMaDatPhong()), String.valueOf(item.getMaPhong()),String.valueOf(item.getMaKhachHang()), String.valueOf(item.getNgayTra()), String.valueOf(item.getNgayDat()), String.valueOf(item.getTongTien())});
+
+                }
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(DocGhiFile.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(DocGhiFile.class
+                            .getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
+    }
+     public List<DatPhong> docFileDatPhong() throws CsvValidationException, ParseException {
+         
+         SimpleDateFormat df =new SimpleDateFormat("dd-MMM-yyyy");
+         
+        FileReader fr = null;
+        List<DatPhong> list = new ArrayList<>();
+        try {
+            fr = new FileReader(PATH_FILE_CSV_DATPHONG);
+            CSVReader csvReader = new CSVReader(fr);
+            String[] line;
+            line = csvReader.readNext();
+            while ((line = csvReader.readNext()) != null) {
+                Date ngayDat = df.parse(line[3]);
+                Date ngayTra = df.parse(line[4]);
+                DatPhong datPhong = new DatPhong(line[0], line[1], line[2],ngayDat,ngayTra,Integer.parseInt(line[5]));
+                list.add(datPhong);
+
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(DocGhiFile.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (fr != null) {
+                try {
+                    fr.close();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(DocGhiFile.class
+                            .getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }
+        return list;
+    }
+     
+}
+
---
+++ De_Tai__so_5/src/de_tai__so_5/TimKiemForm.java
@@ -0,0 +1,224 @@
+package de_tai__so_5;
+
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class TimKiemForm extends javax.swing.JFrame {
+
+    DefaultTableModel tableModel;
+    int id = 0;
+    List<khachHang> danhSachKhachHang = new ArrayList<>();
+    DocGhiFile dc = new DocGhiFile();
+
+    private static final String curentDir = System.getProperty("user.dir");
+    private static final String separator = File.separator;
+    private static final String PATH_FILE_CSV = curentDir + separator + "data" + separator + "KhachHang.csv";
+
+    public TimKiemForm() {
+        initComponents();
+        tableModel = (DefaultTableModel) jTable1.getModel();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        txtTimMA = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        ButunReset4 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(248, 148, 6));
+
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Tìm Kiếm thông tin ");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1777, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        jPanel2.setBackground(new java.awt.Color(48, 62, 80));
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Nhập mã Khách hàng:");
+
+        txtTimMA.setToolTipText("");
+        txtTimMA.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtTimMAActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Mã KH", "Họ Tên", "SĐT", "Tuổi ", "CMND", "PTTT"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        ButunReset4.setBackground(new java.awt.Color(204, 204, 204));
+        ButunReset4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        ButunReset4.setIcon(new javax.swing.ImageIcon("D:\\img\\hienthi.png")); // NOI18N
+        ButunReset4.setText("Hiển thị");
+        ButunReset4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ButunReset4MouseClicked(evt);
+            }
+        });
+        ButunReset4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButunReset4ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addGap(26, 26, 26)
+                .addComponent(txtTimMA, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(ButunReset4))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1792, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(75, 75, 75)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(txtTimMA, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(91, 91, 91)
+                        .addComponent(ButunReset4)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 101, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void txtTimMAActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTimMAActionPerformed
+
+    }//GEN-LAST:event_txtTimMAActionPerformed
+
+    private void ButunReset4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ButunReset4MouseClicked
+        try {
+
+            List<khachHang> list = dc.docFile();
+            boolean flag = false;
+            for (khachHang item : list) {
+                if (item.getMaKhachHang() == txtTimMA.getText()) {
+                    String maKH = item.getMaKhachHang();
+                    String hoTen = item.getHoTen();
+                    int SoCMND = item.getCMND();
+                    int soDT = item.getSDT();
+                    int tuoi = item.getTuoi();
+                    String pttt = item.getPhuongThucThanhToan();
+                    tableModel.addRow(new Object[]{maKH, hoTen, SoCMND, soDT, tuoi, pttt});
+                }
+            }
+            if (flag == false) {
+                JOptionPane.showMessageDialog(rootPane, "Khách hàng không tồn tại");
+            }
+
+        } catch (CsvValidationException ex) {
+            Logger.getLogger(NhapThongTinKhachHangForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }//GEN-LAST:event_ButunReset4MouseClicked
+
+    private void ButunReset4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButunReset4ActionPerformed
+
+    }//GEN-LAST:event_ButunReset4ActionPerformed
+
+    public static void main() {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TimKiemForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TimKiemForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TimKiemForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TimKiemForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TimKiemForm().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ButunReset4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField txtTimMA;
+    // End of variables declaration//GEN-END:variables
+}
